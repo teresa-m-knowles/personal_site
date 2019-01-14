18ee40f637433f9b15883eb7d964a02937eb6786
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-13 23:33:28 -0500

    creates error test and error template:

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_sees_an_error_if_page_does_not_exist
+    visit '/blog'
+
+    assert_equal 404, page.status_code
+    assert page.has_content?("Error")
+  end
 end
