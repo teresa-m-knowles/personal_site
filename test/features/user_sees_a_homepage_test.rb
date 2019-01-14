b45cf78098fc285c32e223a6c00f66e2290061a9
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-14 00:01:17 -0500

    adds first css file

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -17,7 +17,7 @@ class HomepageTest < CapybaraTestCase
 
   def test_user_can_see_about_page
     visit '/about'
-
+    
     assert page.has_content?("About")
     assert_equal 200, page.status_code
   end
