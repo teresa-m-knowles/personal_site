23f4df218df82b6c0ff61c2cf5fca4ff8129c281
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-13 23:43:35 -0500

    adds about test

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -9,9 +9,16 @@ class HomepageTest < CapybaraTestCase
   end
 
   def test_user_sees_an_error_if_page_does_not_exist
-    visit 'blog'
+    visit '/blog'
 
     assert_equal 404, page.status_code
     assert page.has_content?("Page not found")
+  end
+
+  def test_user_can_see_about_page
+    visit '/about'
+
+    assert page.has_content?("About")
+    assert_equal 200, page.status_code
   end
 end
